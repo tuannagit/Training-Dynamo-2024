6930c09d00a627dc6057d80d180072b90706d1ff
tuannagit <s.tuan.work@gmail.com>
2024-06-14 21:33:48 +0900

    lession03_Tuan

--- DesignScriptByTuan.py
+++ DesignScriptByTuan.py
@@ -1,59 +1,62 @@
+# Load the Python Standard and DesignScript Libraries
+import sys
 import clr
-import System
-import math 
-from System.Collections.Generic import *
-
-clr.AddReference("ProtoGeometry")
+clr.AddReference('ProtoGeometry')
 from Autodesk.DesignScript.Geometry import *
 
-clr.AddReference('RevitAPI')
-import Autodesk
-from Autodesk.Revit.DB import *
+#input data
+numPointsX = int(IN[0])
+distancePointsX = IN[1]
+distanceY = IN[2]
 
-clr.AddReference('RevitAPIUI')
-from Autodesk.Revit.UI import*
-from  Autodesk.Revit.UI.Selection import*
+#create list points at X1
+points1 = []
+for i in range(numPointsX):
+    x = 0 + i * distancePointsX
+    point = Point.ByCoordinates(x, 0, 4)
+    points1.append(point)
 
-clr.AddReference("RevitNodes")
-import Revit
-clr.ImportExtensions(Revit.Elements)
-clr.ImportExtensions(Revit.GeometryConversion)
+#create list points at X2
+points2 = []
+for i in range(numPointsX):
+    x = 0 + i * distancePointsX
+    point = Point.ByCoordinates(x, distanceY/2, 4)
+    points2.append(point)
+    
+#create list points at X3
+points3 = []
+for i in range(numPointsX):
+    x = 0 + i * distancePointsX
+    point = Point.ByCoordinates(x, distanceY, 4)
+    points3.append(point)
 
-clr.AddReference("RevitServices")
-import RevitServices
-from RevitServices.Persistence import DocumentManager
-from RevitServices.Transactions import TransactionManager
+#create X-shaped line
+line12 = []
+line21 = []
+line23 = []
+line32 = []
+for i in range((numPointsX)-1):
+    line12.append(Line.ByStartPointEndPoint(points1[i], points2[i+1]))
+    line21.append(Line.ByStartPointEndPoint(points2[i], points1[i+1]))
+    line23.append(Line.ByStartPointEndPoint(points2[i], points3[i+1]))
+    line32.append(Line.ByStartPointEndPoint(points3[i], points2[i+1]))
 
-import clr
-clr.AddReference('DSCoreNodes')
-from DSCore import *
+#get key points    
+pointA = Point.ByCoordinates(0, 0, 4)
+pointA0 = Point.ByCoordinates(0, 0, 0)
+pointB = Point.ByCoordinates((numPointsX-1) * distancePointsX, 0, 4)
+pointB0 = Point.ByCoordinates((numPointsX-1) * distancePointsX, 0, 0)
+pointC = Point.ByCoordinates(0, distanceY, 4)
+pointC0 = Point.ByCoordinates(0, distanceY, 0)
+pointD = Point.ByCoordinates((numPointsX-1) * distancePointsX, distanceY, 4)
+pointD0 = Point.ByCoordinates((numPointsX-1) * distancePointsX, distanceY, 0)
 
+#create vertical lines
+lineA = Line.ByStartPointEndPoint(pointA, pointA0)
+lineB = Line.ByStartPointEndPoint(pointB, pointB0)
+lineC = Line.ByStartPointEndPoint(pointC, pointC0)
+lineD = Line.ByStartPointEndPoint(pointD, pointD0)
 
-
-doc = DocumentManager.Instance.CurrentDBDocument
-view = doc.ActiveView
-uidoc = DocumentManager.Instance.CurrentUIApplication.ActiveUIDocument
-class SelectionFilter(ISelectionFilter):
-	def __init__(self, ctgName1 , ctgName2):
-		self.ctgName1 = ctgName1
-		self.ctgName2 = ctgName2
-
-	def AllowElement(self, element):
-		if element.Category.Name == self.ctgName1 or element.Category.Name == self.ctgName2:
-			return True
-		else:
-			return False
-	def AllowReference(ref, xyZ):
-		return False
-
-
-ele = SelectionFilter("Grids", "Structural Columns")
-elSelectAll = uidoc.Selection.PickElementsByRectangle(ele,"Selects")
-OUT = elSelectAll
-
-grids = []
-column = []
-for i in elSelectAll:
-    if i.Category.Name == "Structural Columns": column.append(i)
-    if i.Category.Name == "Grids": grids.append(i)
-OUT =  column, grids
+#out put
+OUT = pointA, pointB, pointC, pointD, pointA0, pointB0, pointC0, pointD0, line12, line21, line23, line32,lineA, lineB, lineC, lineD
+    
--- script.py
+++ script.py
@@ -1,1 +1,5 @@
-print("hello world")
+nums = [["Mark", "Markdown", "John", "Michael", "Ali"]]
+
+for num in range(len(names)):
+    value = str(names[num]) + "-" + str(num)
+    print(value)
